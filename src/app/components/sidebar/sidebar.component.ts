0a4e0eb215530404db6136e2d15d8ab941990d75
jesirerol <jesirerol@gmail.com>
2022-05-12 14:31:45 +0200

    priemra subida

---
+++ src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+
+declare interface RouteInfo {
+    path: string;
+    title: string;
+    icon: string;
+    class: string;
+}
+export const ROUTES: RouteInfo[] = [
+    { path: '/dashboard', title: 'Empleados',  icon: 'design_app', class: '' },
+    { path: '/icons', title: 'Incidencias',  icon:'education_atom', class: '' },
+    { path: '/maps', title: 'Ausencias',  icon:'location_map-big', class: '' },
+
+    { path: '/upgrade', title: 'Contacto',  icon:'objects_spaceship', class: 'active active-pro' }
+
+];
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent implements OnInit {
+  menuItems: any[];
+
+  constructor() { }
+
+  ngOnInit() {
+    this.menuItems = ROUTES.filter(menuItem => menuItem);
+  }
+  isMobileMenu() {
+      if ( window.innerWidth > 991) {
+          return false;
+      }
+      return true;
+  };
+}
